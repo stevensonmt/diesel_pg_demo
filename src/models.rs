197fd8ed8b42745c69ebc4d90b0bd777f0807edb
Matthew Stevenson <stevensonmt@gmail.com>
2019-01-29 22:29:06 -0800

    second commit

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,13 @@
+use super::schema::posts;
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+}
+
+
 #[derive(Queryable)]
 pub struct Post {
     pub id: i32,
